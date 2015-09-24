cd72f4780b8118f4efa077d6103792ef80a068d8
Li Zheng <flyskywhy@gmail.com>
2015-09-24 16:13:08 +0800

    t.sh: auto sed the device id

--- t.sh
+++ t.sh
@@ -68,6 +68,10 @@ local_sed ()
     sed -i "s/id=\"default\" path=\"\/home\/syncthing\/Sync\"/id=\"$folder\" path=\"\/sync\"/" $folder/config/config.xml
     sed -i "s/urAccepted>0/urAccepted>-1/" $folder/config/config.xml
 
+    grep "^        <device id=" $folder/config/config.xml
+    sed -i "/^        <device id=/r s_folder_device_id" $folder/config/config.xml
+    sed -i "/^    <\/device>/r s_device_id" $folder/config/config.xml
+
     echo
 }
 
